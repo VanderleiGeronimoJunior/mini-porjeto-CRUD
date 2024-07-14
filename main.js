4c8fff965ae01be6b257617fcb82c00e60740ed7
Vanderlei Geronimo Junior <vand.9jr@gmail.com>
2024-07-14 10:43:21 -0300

    concluido creat e read do crud, iniciandi update

--- main.js
+++ main.js
@@ -6,8 +6,34 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    name: "Abela",
+    email: "abela@gmail.com",
+    celular: "48999565538",
+    cidade: "Araranguá"
+    }
+
+
+
+
+const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient))
 
 // CRUD
+
+// UPDATE
+const updateClient = (client) => {
+    
+}
+
+// READ
+const readClient = () => JSON.parse(localStorage.getItem("dbClient")) ?? []
+
+// CREATE
+const createClient = (client) => {
+    const dbClient = readClient()
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+}
 
 
 
